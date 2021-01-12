9006a0e632727b89e6ff40481979c6fc9514ea38
Vinicius Oliveira Souza <viniciusos@take.net>
2021-01-12 10:05:38 -0300

    feat: move configs to js and generate css classes

---
+++ config.js
@@ -0,0 +1,109 @@
+const StyleDictionary = require("style-dictionary").extend(
+  {
+    source: ["properties/**/*.json"],
+    platforms: {
+      json: {
+        transformGroup: "web",
+        buildPath: "build/json/",
+        files: [
+          {
+            destination: "variables.json",
+            format: "json/flat",
+          },
+        ],
+      },
+      scss: {
+        transformGroup: "scss",
+        buildPath: "build/scss/",
+        files: [
+          {
+            destination: "variables.scss",
+            format: "scss/variables",
+            filter: {
+              attributes: {
+                category: "color",
+              },
+            },
+          },
+        ],
+      },
+      css: {
+        buildPath: "build/css/",
+        files: [
+          {
+            destination: "classes.css",
+            format: "css/variables",
+          },
+        ],
+      },
+      "assets/embed/json": {
+        transforms: ["attribute/cti", "name/cti/kebab", "asset/base64"],
+        buildPath: "build/json/",
+        files: [
+          {
+            destination: "assets_icons.json",
+            format: "json/flat",
+            filter: {
+              attributes: {
+                category: "asset",
+                type: "icon",
+              },
+            },
+          },
+          {
+            destination: "assets_emojis.json",
+            format: "json/flat",
+            filter: {
+              attributes: {
+                category: "asset",
+                type: "emoji",
+              },
+            },
+          },
+          {
+            destination: "colors.json",
+            format: "json/flat",
+            filter: {
+              attributes: {
+                category: "color",
+              },
+            },
+          },
+        ],
+      },
+    },
+  },
+  {
+    source: ["properties/**/*.json"],
+    platforms: {
+      json: {
+        transformGroup: "web",
+        buildPath: "build/json/",
+        files: [
+          {
+            destination: "variables.json",
+            format: "json/flat",
+          },
+        ],
+      },
+    },
+  }
+);
+
+StyleDictionary.registerFormat({
+  name: "css/variables",
+  formatter: function (dictionary) {
+    const properties = dictionary.properties.color;
+
+    const colors = Object.keys(properties)
+      .map((prop) => `.color-${prop} { color: ${properties[prop].value}; }`)
+      .join("\n");
+    const backgrounds = Object.keys(properties)
+      .map((prop) => `.bg-${prop} { background: ${properties[prop].value}; }`)
+      .join("\n");
+
+    return colors.concat(backgrounds);
+  },
+});
+
+StyleDictionary.buildAllPlatforms();
